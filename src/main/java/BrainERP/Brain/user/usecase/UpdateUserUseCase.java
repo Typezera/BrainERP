3d410b07387600a963f9661a61fdf4af31dbccc2
Typezera <willianbardela2131@hotmail.com>
2026-06-11 21:09:26 -0300

    refactor: criando uma função de validação para o UpdateUserUseCase

--- src/main/java/BrainERP/Brain/user/usecase/UpdateUserUseCase.java
+++ src/main/java/BrainERP/Brain/user/usecase/UpdateUserUseCase.java
@@ -2,17 +2,21 @@ package BrainERP.Brain.user.usecase;
 
 import BrainERP.Brain.user.dto.UserPatchDto;
 import BrainERP.Brain.user.dto.UserResponseDto;
+import BrainERP.Brain.user.model.UserModel;
 import BrainERP.Brain.user.repository.UserRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UpdateUserUseCase {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UpdateUserUseCase(UserRepository userRepository){
+    public UpdateUserUseCase(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserResponseDto userPatch(Long id, UserPatchDto userPatchDto){
@@ -21,9 +25,35 @@ public class UpdateUserUseCase {
                         HttpStatus.NOT_FOUND, "Usuário não encontrado"
                 ));
 
-        if (userPatchDto.name() != null && !userPatchDto.name().isBlank()){user.setName(userPatchDto.name());}
-        if (userPatchDto.email() != null && !userPatchDto.email().isBlank()){user.setEmail(userPatchDto.email());}
-        if (userPatchDto.password() != null && !userPatchDto.password().isBlank()){user.setPassword(userPatchDto.password());}
+        return appyPatch(user, userPatchDto);
+    }
+
+    public UserResponseDto appyPatch(UserModel user, UserPatchDto userPatch){
+
+        boolean updated = false;
+
+        if (userPatch.name() != null && !userPatch.name().isBlank()) {
+            user.setName(userPatch.name());
+            updated = true;
+        }
+
+        if (userPatch.email() != null && !userPatch.email().isBlank()) {
+            user.setEmail(userPatch.email());
+            updated = true;
+        }
+
+        if (userPatch.password() != null && !userPatch.password().isBlank()) {
+            user.setPassword(
+                    passwordEncoder.encode(userPatch.password())
+            );
+            updated = true;
+        }
+
+        if (!updated){
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Nenhum valor valido para atualizar"
+            );
+        }
 
         var updatedUser = userRepository.save(user);
 
@@ -35,4 +65,6 @@ public class UpdateUserUseCase {
                 updatedUser.getHowAreYou()
         );
     }
+
+
 }
