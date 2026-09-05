11cce3c9b1e57c8c042edc0c9d09da676b23d07e
Typezera <willianbardela2131@hotmail.com>
2026-09-05 19:20:38 -0300

    feat: criação de UserSecurityService para checkagem do contexto do JWT do usuário logado x usuário que vai ser alterado

---
+++ src/main/java/BrainERP/Brain/user/service/UserSecurityService.java
@@ -0,0 +1,58 @@
+package BrainERP.Brain.user.service;
+
+import BrainERP.Brain.user.model.UserModel;
+import BrainERP.Brain.user.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class UserSecurityService {
+    private final UserRepository userRepository;
+
+    public UserSecurityService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    private UserModel getLoggedUser(){
+        Authentication authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+
+        Jwt jwt =
+                (Jwt) authentication.getPrincipal();
+
+        String accountType = jwt.getClaim("accountType");
+
+        if (!"USER".equals(accountType)){
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN,
+                    "Apenas um usuário pode fazer essa operação"
+            );
+        }
+
+        Long userId = jwt.getClaim("id");
+
+        return userRepository.findByIdAndActivateTrue(userId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "Usuário não encontrado"
+                ));
+
+    }
+
+    public void checkRealUser(UserModel user){
+        var userLogged = getLoggedUser();
+
+        if (!user.getId().equals(userLogged.getId())){
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN,
+                    "Você não tem permissão para alterar esse usuário."
+            );
+        }
+    }
+}
--- src/main/java/BrainERP/Brain/user/usecase/DeleteUserUseCase.java
+++ src/main/java/BrainERP/Brain/user/usecase/DeleteUserUseCase.java
@@ -1,6 +1,7 @@
 package BrainERP.Brain.user.usecase;
 
 import BrainERP.Brain.user.repository.UserRepository;
+import BrainERP.Brain.user.service.UserSecurityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,8 +10,12 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class DeleteUserUseCase {
     private final UserRepository userRepository;
+    private final UserSecurityService userSecurityService;
 
-    public DeleteUserUseCase(UserRepository userRepository){this.userRepository = userRepository;}
+    public DeleteUserUseCase(UserRepository userRepository, UserSecurityService userSecurityService){
+        this.userRepository = userRepository;
+        this.userSecurityService = userSecurityService;
+    }
 
     @Transactional
     public void deactivateUser(Long id){
@@ -18,6 +23,9 @@ public class DeleteUserUseCase {
                 .orElseThrow(() -> new ResponseStatusException(
                         HttpStatus.NOT_FOUND, "Usuario não existe"
                 ));
+
+        userSecurityService.checkRealUser(user);
+
         user.setActivate(false);
     }
 }
